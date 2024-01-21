0a761d9fe39a9fcbd711e3c3a67ca4ac6a80841f
IgorFe <greusunset@mail.ru>
2024-01-21 20:26:41 +0700

    first_homework.py

--- test/test_firsthomework.py
+++ test/test_firsthomework.py
@@ -8,7 +8,7 @@ class TestSolve(unittest.TestCase):
     def test_solve(self):
         self.assertEqual(solve(1, 0, 1), ())
         self.assertEqual(solve(1, 0, -1), (1, -1))
-        self.assertEqual(solve(1, 2, 0.999998581), (-1, -1))
+        self.assertEqual(solve(1, 2, 0.999998580), (-1, -1))
         self.assertRaises(ValueError, solve, 0, 3, 5)
         self.assertRaises(ValueError, solve, nan, nan, nan)
         self.assertRaises(ValueError, solve, inf, -inf, inf)
